23fe9efdd380edf37ffa8830415ff36397a2a246
FujimuraKaito <>
2021-03-13 14:22:13 +0900

    :memo: add some methods

--- flutter_bloc/bloc_basic_app/lib/bloc/counter_bloc.dart
+++ flutter_bloc/bloc_basic_app/lib/bloc/counter_bloc.dart
@@ -1,26 +1,31 @@
 import 'dart:async';
 
+class CountData {
+  int count;
+  int pushCount;
+
+  CountData({this.count, this.pushCount});
+}
+
 class CounterBloc {
   // ボタンによるカウントアップ入力を受け付ける
-  final _actionController = StreamController<bool>();
+  final _actionController = StreamController<CountData>();
   Sink<void> get increment => _actionController.sink;
 
   // これにカウントアップした値を流す
-  final _counterController = StreamController<int>();
-  Stream<int> get count => _counterController.stream;
+  final _counterController = StreamController<CountData>();
+  Stream<CountData> get count => _counterController.stream;
 
-  int _count = 0;
+  int _count = 1;
+  int _pushCount = 0;
 
   CounterBloc() {
-    _actionController.stream.listen((shingichi) {
-      if (shingichi) {
-        _count++;
-        print(_count);
-        // ここか実行されることで画面に反映される
-        _counterController.sink.add(_count);
-      } else {
-        print('カウントアップしません');
-      }
+    _actionController.stream.listen((countData) {
+      _count += countData.count;
+      _pushCount += countData.pushCount;
+      // ここか実行されることで画面に反映される
+      _counterController.sink
+          .add(CountData(count: _count, pushCount: _pushCount));
     });
   }
 
--- flutter_bloc/bloc_basic_app/lib/main.dart
+++ flutter_bloc/bloc_basic_app/lib/main.dart
@@ -39,6 +39,7 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     // 子Widgetで呼ぶ
     final counterBloc = Provider.of<CounterBloc>(context);
+    int num = 1;
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
@@ -54,24 +55,25 @@ class MyHomePage extends StatelessWidget {
             // 実際に値を表示するところ
             // StreamBuilderでBLoCの値を受け取る
             StreamBuilder(
-              initialData: 0,
+              initialData: CountData(count: 1, pushCount: 0),
               // blocクラスでstreamに指定したほう
               stream: counterBloc.count,
               builder: (context, snapshot) {
+                num = snapshot.data.count;
                 return Text(
-                  '${snapshot.data}',
+                  '${snapshot.data.count}(2^${snapshot.data.pushCount})',
                   style: Theme.of(context).textTheme.headline2,
                 );
               },
-            )
+            ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           // Blocの受け入れ部分(Sink<T>)にaddする
-          print('ボタンが押されました');
-          counterBloc.increment.add(true);
+          CountData countData = CountData(count: num, pushCount: 1);
+          counterBloc.increment.add(countData);
         },
         tooltip: 'Increment',
         child: Icon(Icons.add),
